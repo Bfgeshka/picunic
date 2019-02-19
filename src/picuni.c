1f2b3da9d94733b213819697228e64f929a83f8e
Bfgeshka <bfgeshka@aaathats3as.com>
2019-02-19 15:26:34 +0900

    picuni.c: load directory content

--- src/picuni.c
+++ src/picuni.c
@@ -1,10 +1,15 @@
-#include <stdlib.h>
 #include "application.h"
+#include "files.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 int
 main ( int argc, char ** argv )
 {
 	A_args( argc, argv );
+	F_load_all_paths();
 
+	F_finish();
+	fputs( "All good, exitting...\n", stderr );
 	return EXIT_SUCCESS;
 }
