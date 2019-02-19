7cb6732d42830eab542de7930436c319fa9367b0
Bfgeshka <bfgeshka@aaathats3as.com>
2019-02-19 16:09:18 +0900

    files.c: recursive walking

--- src/files.c
+++ src/files.c
@@ -1,11 +1,55 @@
+/* Macros */
+#include "config.h"
 #include "files.h"
 #include "stringutils.h"
 #include <dirent.h>
 #include <stdio.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 
+/* Local scope */
+static void S_F_load_path ( char * path );
 
+static void S_F_load_path ( char * path )
+{
+		fprintf( stderr, "Loading contents of %s...\n", path );
+
+		DIR * curdir = opendir(path);
+		if ( curdir == NULL )
+		{
+			fprintf( stderr, "Cannot open %s, skipping...\n", path );
+			return;
+		}
+
+		size_t dirsize = 0;
+		struct dirent * entry = NULL;
+		string * str = construct_string(4096);
+
+		while ( ( entry = readdir(curdir) ) )
+		{
+			if ( strcmp( entry->d_name, "." ) == 0 || strcmp( entry->d_name, ".." ) == 0 )
+				continue;
+
+			stringset( str, "%s/%s", path, entry->d_name );
+
+			struct stat st;
+			if ( stat( str->s, &st ) == 0 )
+			{
+				if ( S_ISREG(st.st_mode) )
+					dirsize++;
+
+				if ( config.recursive && S_ISDIR(st.st_mode) )
+					S_F_load_path(str->s);
+			}
+		}
+
+		fprintf( stderr, "Usable files in %s: %zu\n", path, dirsize );
+		free_string(str);
+		closedir(curdir);
+}
+
+/* Global scope */
 void
 F_check_path ( char * path )
 {
@@ -28,36 +72,6 @@ F_finish ( void )
 void
 F_load_all_paths ( void )
 {
-	string * str = construct_string(4096);
-
 	for ( size_t i = 0; i < directories.count; ++i )
-	{
-		char * curpath = directories.value[i];
-		fprintf( stderr, "Loading contents of %s...\n", curpath );
-
-		DIR * curdir = opendir(curpath);
-		if ( curdir == NULL )
-		{
-			fprintf( stderr, "Cannot open %s, skipping...\n", curpath );
-			continue;
-		}
-
-		size_t dirsize = 0;
-		struct dirent * entry = NULL;
-		while ( ( entry = readdir(curdir) ) )
-		{
-			stringset( str, "%s/%s", curpath, entry->d_name );
-
-			struct stat st;
-			if ( stat( str->s, &st ) == 0 && S_ISREG(st.st_mode) )
-			{
-				dirsize++;
-			}
-		}
-
-		fprintf( stderr, "Usable files in directory: %zu\n", dirsize );
-		closedir(curdir);
-	}
-
-	free_string(str);
+		S_F_load_path(directories.value[i]);
 }
