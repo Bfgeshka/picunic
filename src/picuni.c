ea7ba037b90227c2de7e05c52c916b6191b67382
Bfgeshka <bfgeshka@aaathats3as.com>
2019-02-25 10:26:40 +0900

    picuni.c: show stats after scanning

--- src/picuni.c
+++ src/picuni.c
@@ -1,5 +1,6 @@
 #include "application.h"
 #include "files.h"
+#include "image.h"
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -7,9 +8,10 @@ int
 main ( int argc, char ** argv )
 {
 	A_args( argc, argv );
-
 	F_cook_regexp();
+
 	F_load_all_paths();
+	I_stats();
 
 	F_finish();
 	fputs( "All good, exitting...\n", stderr );
