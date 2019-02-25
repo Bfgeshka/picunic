929d6d63a05273959d36d7a41a6f3e070e276707
Bfgeshka <bfgeshka@aaathats3as.com>
2019-02-25 14:52:44 +0900

    image.c: remove medium function

--- src/image.c
+++ src/image.c
@@ -8,7 +8,6 @@
 /* Local scope */
 static imagelist List;
 
-static void S_I_check ( string * path );
 static unsigned long S_I_get_mean ( PixelPacket * pixels );
 static void S_I_add_to_list ( img * image );
 static void S_I_compare ( img * img1, img * img2 );
@@ -80,57 +79,6 @@ S_I_get_mean ( PixelPacket * pixels )
 	return colr / config.square;
 }
 
-static void
-S_I_check ( string * instr )
-{
-	ExceptionInfo ex;
-	ImageInfo * image_info;
-	Image * image;
-	Image * resized_image;
-
-	string * str = stringcopy(instr);
-
-	InitializeMagick(NULL);
-	GetExceptionInfo(&ex);
-
-	image_info = CloneImageInfo((ImageInfo *)NULL);
-	strcpy( image_info->filename, str->s );
-
-	image = ReadImage( image_info, &ex );
-
-	if ( ex.severity != UndefinedException )
-		CatchException(&ex);
-
-	if ( !image )
-	{
-		fputs( "Null image data.\n", stderr );
-
-		free_string(str);
-		goto S_I_check_onexit;
-	}
-
-	resized_image = ResizeImage( image, config.avghash_side, config.avghash_side, LanczosFilter, 1.0 ,&ex );
-	DestroyImage(image);
-
-	if ( resized_image == (Image *)NULL )
-	{
-		fputs( "Failed to resize.\n", stderr );
-		CatchException(&ex);
-
-		free_string(str);
-		goto S_I_check_onexit;
-	}
-
-	GrayscalePseudoClassImage( resized_image, 1 );
-	S_I_hash_from_thumb( resized_image, str );
-	DestroyImage(resized_image);
-
-	S_I_check_onexit:
-	DestroyImageInfo(image_info);
-	DestroyExceptionInfo(&ex);
-	DestroyMagick();
-}
-
 /* Global scope */
 void
 I_stats ( void )
@@ -181,7 +129,52 @@ I_compare_all ( void )
 }
 
 void
-I_process ( string * path )
+I_process ( string * instr )
 {
-	S_I_check(path);
+	ExceptionInfo ex;
+	ImageInfo * image_info;
+	Image * image;
+	Image * resized_image;
+
+	string * str = stringcopy(instr);
+
+	InitializeMagick(NULL);
+	GetExceptionInfo(&ex);
+
+	image_info = CloneImageInfo((ImageInfo *)NULL);
+	strcpy( image_info->filename, str->s );
+
+	image = ReadImage( image_info, &ex );
+
+	if ( ex.severity != UndefinedException )
+		CatchException(&ex);
+
+	if ( !image )
+	{
+		fputs( "Null image data.\n", stderr );
+
+		free_string(str);
+		goto S_I_check_onexit;
+	}
+
+	resized_image = ResizeImage( image, config.avghash_side, config.avghash_side, LanczosFilter, 1.0 ,&ex );
+	DestroyImage(image);
+
+	if ( resized_image == (Image *)NULL )
+	{
+		fputs( "Failed to resize.\n", stderr );
+		CatchException(&ex);
+
+		free_string(str);
+		goto S_I_check_onexit;
+	}
+
+	GrayscalePseudoClassImage( resized_image, 1 );
+	S_I_hash_from_thumb( resized_image, str );
+	DestroyImage(resized_image);
+
+	S_I_check_onexit:
+	DestroyImageInfo(image_info);
+	DestroyExceptionInfo(&ex);
+	DestroyMagick();
 }
