c6c86636f4575c344ae896b5d53a61b9fd65d4ac
Bfgeshka <bfgeshka@aaathats3as.com>
2019-02-25 10:25:52 +0900

    image.h: structs

--- include/image.h
+++ include/image.h
@@ -1,13 +1,30 @@
 #ifndef IMAGE_H_
 #define IMAGE_H_
 
+/* Macros */
+#include <stdint.h>
+#include <inttypes.h>
+#include <stdlib.h>
+
 /* Typedef */
 typedef struct img
 {
 	char * path;
-	unsigned long long hash;
+	uint_fast64_t hash;
+
+	struct img * prev;
+	struct img * next;
 } img;
 
+typedef struct imagelist
+{
+	size_t length;
+	img * head;
+	img * tail;
+} imagelist;
+
+/* Protos */
 void I_process ( char * path );
+void I_stats ( void );
 
 #endif
