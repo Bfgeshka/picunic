2330ba467af2fc156bca0707afdfdc5563dfa0fe
Bfgeshka <bfgeshka@aaathats3as.com>
2019-02-25 15:45:52 +0900

    image.h: move typedefs to imagelist.h

--- include/image.h
+++ include/image.h
@@ -3,39 +3,6 @@
 
 /* Macros */
 #include "stringutils.h"
-#include <stdint.h>
-#include <inttypes.h>
-#include <stdlib.h>
-
-/* Typedef */
-typedef struct img
-{
-	string * path;
-	uint_fast64_t hash;
-
-	struct img * prev;
-	struct img * next;
-} img;
-
-typedef struct imagelist
-{
-	unsigned long length;
-	img * head;
-	img * tail;
-} imagelist;
-
-typedef struct simgroup
-{
-	uint_fast64_t grouphash;
-	imagelist list;
-} simgroup;
-
-typedef struct simgrouplist
-{
-	unsigned long length;
-	simgroup * head;
-	simgroup * tail;
-} simgrouplist;
 
 /* Protos */
 void I_process ( string * path );
